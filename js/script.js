66b8a7003b5b3d4fa959585a98918213b3a7287e
Carson Hansen <92325833+Cesium1289@users.noreply.github.com>
2024-01-30 13:26:44 -0800

    Display popular movies on homescreen of app
    -Display popular movies on movies page

---
+++ js/script.js
@@ -0,0 +1,77 @@
+const global = { currentPage: window.location.pathname };
+
+function HightlightActiveLink(e) {
+  const activeLink = document.querySelectorAll(".nav-link");
+  activeLink.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage)
+      link.classList.add("active");
+  });
+}
+async function FetchAPIData(endpoint) {
+  const API_KEY = KEY;
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = response.json();
+  return data;
+}
+async function DisplayPopularMoviesToDOM() {
+  const { results } = await FetchAPIData("movie/popular");
+  console.log(results);
+  const list = document.querySelector("#popular-movies");
+  results.forEach((movie, x) => {
+    div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+     ${
+       movie.poster_path
+         ? ` <img
+     src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+     class="card-img-top"
+     alt="${movie.title}"
+   />`
+         : `<img
+   src="images/no-image.jpg"
+   class="card-img-top"
+   alt="${movie.title}"
+ />`
+     }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>
+  `;
+    console.log(div);
+    list.appendChild(div);
+  });
+}
+
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      GetPopularMovies();
+      break;
+    case "/shows.html":
+      console.log("shows");
+      break;
+    case "/movie-details.html":
+      console.log("movie details");
+      break;
+    case "/tv-details.html":
+      console.log("tv details");
+      break;
+    case "/search.html":
+      console.log("search");
+      break;
+  }
+  HightlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
