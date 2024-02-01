2161a8a53e4215f90490439a7a00de9a146f0164
Carson Hansen <92325833+Cesium1289@users.noreply.github.com>
2024-02-01 10:32:02 -0800

    Display movie details
    -Added ability to view details about a movie that is selected
    -Removed template movies detail section

--- js/script.js
+++ js/script.js
@@ -11,10 +11,13 @@ async function FetchAPIData(endpoint) {
   const API_KEY = KEY;
   const API_URL = "https://api.themoviedb.org/3/";
 
+  ShowSpinner();
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = response.json();
+
+  HideSpinner();
   return data;
 }
 
@@ -54,6 +57,76 @@ async function DisplayPopularShowsToDOM() {
   });
 }
 
+async function DisplayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await FetchAPIData(`movie/${movieId}`);
+  console.log(movie);
+  const div = document.createElement("div");
+  movie.genres.forEach((genre) => console.log(genre.name));
+  div.innerHTML = ` <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? ` <img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+      />`
+      : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+      />`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${AddCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${AddCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group"> <span>  ${movie.production_companies.map(
+    (company) => " " + company.name
+  )}
+  </span>
+</div>`;
+
+  document.querySelector("#movie-details").appendChild(div);
+}
+
+function AddCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 async function DisplayPopularMoviesToDOM() {
   const { results } = await FetchAPIData("movie/popular");
   console.log(results);
@@ -89,6 +162,13 @@ async function DisplayPopularMoviesToDOM() {
   });
 }
 
+function ShowSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+function HideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
 function init() {
   switch (global.currentPage) {
     case "/":
@@ -99,7 +179,7 @@ function init() {
       DisplayPopularShowsToDOM();
       break;
     case "/movie-details.html":
-      console.log("movie details");
+      DisplayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("tv details");
