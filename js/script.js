7f03ab41bba13cb071f1d5b2dd16aa62e56b372b
Carson Hansen <92325833+Cesium1289@users.noreply.github.com>
2024-02-05 12:24:43 -0800

    Added search functionality
    -Added the ability to search for shows and movies
    -Added an animated slider to show current popular movies

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-const global = { currentPage: window.location.pathname };
+const global = {
+  currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+    totalResults: 0,
+  },
+};
 
 function HightlightActiveLink(e) {
   const activeLink = document.querySelectorAll(".nav-link");
@@ -56,6 +65,154 @@ async function DisplayPopularShowsToDOM() {
     list.appendChild(div);
   });
 }
+async function SearchApiData() {
+  const API_KEY = KEY;
+  const API_URL = "https://api.themoviedb.org/3/";
+  ShowSpinner();
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+  const data = await response.json();
+  HideSpinner();
+  return data;
+}
+async function Search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    const { results, total_pages, page, total_results } = await SearchApiData();
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    if (results.length === 0) {
+      ShowAlert("No results found");
+      return;
+    }
+    console.log(document.querySelector("#search-term").value);
+    DisplaySearchResults(results);
+    //document.querySelector("#search-term").value = "";
+  } else {
+    ShowAlert("Enter a search term");
+  }
+}
+
+function DisplaySearchResults(results) {
+  console.log(document.querySelector("#search-term").value);
+  results.forEach((result, x) => {
+    div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="${global.search.type}-details.html?id=${result.id}">
+     ${
+       result.poster_path
+         ? ` <img
+     src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+     class="card-img-top"
+     alt="${global.search.type === "movie" ? result.title : result.name}"
+   />`
+         : `<img
+   src="images/no-image.jpg"
+   class="card-img-top"
+   alt="${global.search.type === "movie" ? result.title : result.name}"
+ />`
+     }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${
+        global.search.type === "movie" ? result.title : result.name
+      }</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${
+          global.search.type === "movie"
+            ? result.release_date
+            : result.first_air_date
+        }</small>
+      </p>
+    </div>
+  `;
+    console.log(div);
+    document.querySelector("#search-results-heading").innerHTML = `<h2>Found ${
+      global.search.totalResults
+    } results for the ${global.search.term} ${
+      document.querySelector("#search-term").value
+    } <\h2>`;
+    document.querySelector("#search-results").appendChild(div);
+  });
+}
+
+function ShowAlert(message, className = "error") {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
+//Display slider movies
+async function DisplaySlider() {
+  const { results } = await FetchAPIData("movie/now_playing");
+  console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    <div>
+    ${
+      movie.poster_path
+        ? ` <img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+        />`
+        : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+        />`
+    }
+    </div>
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+        1
+      )} / 10
+    </h4>
+ `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+    InitSwiper();
+  });
+}
+
+function InitSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
 
 //display backdrop of details page for movies or shows
 async function DisplayBackgroundImage(type, path) {
@@ -81,9 +238,7 @@ async function DisplayMovieDetails() {
   const movieId = window.location.search.split("=")[1];
   const movie = await FetchAPIData(`movie/${movieId}`);
   DisplayBackgroundImage("movie", movie.backdrop_path);
-  console.log(movie);
   const div = document.createElement("div");
-  movie.genres.forEach((genre) => console.log(genre.name));
   div.innerHTML = ` <div class="details-top">
   <div>
   ${
@@ -260,6 +415,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      DisplaySlider();
       DisplayPopularMoviesToDOM();
       break;
     case "/shows.html":
@@ -272,7 +428,7 @@ function init() {
       DisplayShowDetails();
       break;
     case "/search.html":
-      console.log("search");
+      Search();
       break;
   }
   HightlightActiveLink();
